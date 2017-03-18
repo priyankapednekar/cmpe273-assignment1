ece0aad6edc3d1c9d102e04b66332253bd67c5e0
priyankapednekar <priyankapednekar4@gmail.com>
2017-03-17 23:55:03 -0700

    credentail modify

--- app.py
+++ app.py
@@ -18,8 +18,8 @@ def hello(file_name):
 	repo_name=file_path[25:]
 	file_type= file_name.split(".")
 	
-	g=Github(login_or_token="c33593dad5481122105464469ca45ee566bdaa4e",base_url=BASE_URL)
-	#g=Github(base_url=BASE_URL)
+	#g=Github(login_or_token="c33593dad5481122105464469ca45ee566bdaa4e",base_url=BASE_URL)
+	g=Github(base_url=BASE_URL)
 	git=g.get_user(user_name)
 	repo = git.get_repo(repo_name)
 	
@@ -33,7 +33,7 @@ def hello(file_name):
 		elif (file_type[1]=='json'):
 			f=file_type[0]+'.yml'
 			con_json= repo.get_contents(f)
-			raw= con_json.decoded_content 
+			raw = con_json.decoded_content 
 			return json.dumps(yaml.load(raw))
 
 	except:
